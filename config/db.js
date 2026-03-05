0433f02c093d75ace1927c7a59afd93bd8c85f22
Frictious2 <gegbaidavidm@gmail.com>
2026-03-05 12:15:59 +0000

    DB env

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'personnel_master',
+  host: process.env.DBHOST || '127.0.0.1',
+  user: process.env.DBUSER || 'root',
+  password: process.env.DBPASS || '',
+  database: process.env.DBNAME || 'personnel_master',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
